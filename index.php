231aa3353eacaace7edfd297231e38c751a4831b
Moise NIZEYIMANA <59395205+KNMoise@users.noreply.github.com>
2025-09-04 01:05:44 +0200

    Modify favicon links in index.php
    Updated favicon links and commented out the old shortcut icon.

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ require_once 'includes/functions.php';
     <title>Lions Design - Premium Products & Gifts</title>
     <link rel="icon" type="image/png" href="favicon-96x96.png" sizes="96x96" />
     <link rel="icon" type="image/svg+xml" href="favicon.svg" />
-    <link rel="shortcut icon" href="favicon.ico" />
+    <!-- <link rel="shortcut icon" href="favicon.ico" /> -->
+    <link rel="icon" href="/favicon/favicon.ico" type="image/x-icon">
     <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
     <link rel="manifest" href="site.webmanifest" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
